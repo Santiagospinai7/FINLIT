5d6a4f7b443bca947931f1633f83beeb31dea1bc
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-03-07 11:21:37 +0000

    create dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/dashboard", to: "pages#dashboard"
+
   resources :accounts do
     resources :transactions
   end
