ba83fc334eb57ad5bf3902b0bf8ded0f1a7c000d
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-03-07 10:47:32 +0000

    space

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -24,7 +24,6 @@ class AccountsController < ApplicationController
   def create
     @account = Account.new(account_params)
     @account.user_id = current_user.id
-
     @account.save
 
     redirect_to accounts_path
