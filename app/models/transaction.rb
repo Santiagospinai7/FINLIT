2662bb17f627eec8462e2d49071805c12a46db19
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-03-09 16:39:58 +0000

    add line_char to the account dashboard

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,14 @@
 class Transaction < ApplicationRecord
   belongs_to :account
+
+  def self.line_chart(account)
+    transactions = account.transactions
+    transactions_by_date = transactions.group(:date).sum(:amount)
+
+    formatted_transactions_by_date = transactions_by_date.map do |date, amount|
+      [date.strftime("%Y-%m-%d"), amount]
+    end.to_h
+
+    return formatted_transactions_by_date
+  end
 end
