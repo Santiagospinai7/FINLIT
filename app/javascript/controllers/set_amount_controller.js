4a12a4c905b51358f70b0c449f7b300a06db1bca
Timothy Popoola <timothypopoola123@outlook.com>
2023-03-09 12:29:40 +0000

    fixed bug with amount input on transaction page

--- app/javascript/controllers/set_amount_controller.js
+++ app/javascript/controllers/set_amount_controller.js
@@ -2,40 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="set-amount"
 export default class extends Controller {
-  let expenseLabelActive = false;
+  static targets = ["income-label", "expense-label", "amountInput"]
 
 
   connect() {
-    console.log("hello from stimulus")
-
+    this.setIncome();
   }
 
   // Create a listener change for when the user is typping in the amount field
   //creste event list that calls below function
 
-  transformAmount(data) {
-    if (expenseLabelActive) {
-      return \+amount\
-    } else {
-      return \-amount\
-    }
+  setIncome() {
+    console.log("Income");
+    // remove attributes from setExpense if there
+    this.amountInputTarget.setAttribute("min", "1")
+    this.amountInputTarget.setAttribute("onkeypress", "return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))")
   }
 
-  setTrue() {
-    console.log("setting true");
-    let expenseLabelActive = true;
-    // income
-    // convert the amount in positive (add a '+' before the amount input)
+  setExpense() {
+    console.log("Expense");
+    // remove attributes added from set income
+
+    // add attributes that do the opposite from setIncome
+    this.amountInputTarget.setAttribute("max", "0")
 
   }
-
-  setFalse() {
-    console.log("setting false");
-    let expenseLabelActive = false;
-    // expense
-
-    // convert the amount in negative (add a '-' before the amount input)
-
-  }
-
-  }
+}
