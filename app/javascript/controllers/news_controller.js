512678f78896a83e76926028cf2cfbc5d61494d4
max whitehead <fifes-catkin.0r@icloud.com>
2023-03-10 22:39:41 +0000

    NEWS UPDATES

--- app/javascript/controllers/news_controller.js
+++ app/javascript/controllers/news_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "title", "link" ]
+  static targets = [ "title", "link", "source"]
   data = [];
 
   connect() {
@@ -18,6 +18,8 @@ export default class extends Controller {
         this.data = data;
         const firstNewsItem = data[0];
         const firstNewsLink = data[0].link;
+        const firstNewsSource = data[0].source;
+        this.sourceTarget.innerText = firstNewsSource;
         this.titleTarget.innerText = firstNewsItem.title;
         this.linkTarget.href = firstNewsLink;
       })
@@ -26,10 +28,23 @@ export default class extends Controller {
 
   refresh() {
     console.log("click");
-    const randomIndex = Math.floor(Math.random() * this.data.length);
-    const randomNewsItem = this.data[randomIndex];
+    const shuffledData = this.shuffle(this.data); // shuffle the data array
+    const randomIndex = Math.floor(Math.random() * shuffledData.length);
+    const randomNewsItem = shuffledData[randomIndex];
     const randomNewsLink = randomNewsItem.link;
+    const randomNewsSource = randomNewsItem.source;
     this.titleTarget.innerText = randomNewsItem.title;
     this.linkTarget.href = randomNewsLink;
+    this.sourceTarget.innerText = randomNewsSource;
   }
+
+  shuffle(array) {
+    // Fisher-Yates shuffle algorithm
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
 }
