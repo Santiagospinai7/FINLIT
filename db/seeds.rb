dcca30732920dd78d3e2a4f3be1836dec345bbcf
Timothy Popoola <timothypopoola123@outlook.com>
2023-03-13 11:37:25 +0000

    learning materials backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -106,11 +106,79 @@ random_names = ["Bestie", "BFF", "Queen", "Senorita", "Girly", "Gal", "Friend", 
 #   user_id: user5.id
 # )
 
-investment = TopicMaterial.create(topic_name: "investment")
+crypto_guide = TopicMaterial.create(topic_name: "cryptocurrency")
 
-LearningMaterial.create(title: "how can you invest",
-  description: "An explanation...",
-  topic_material_id: investment.id
+LearningMaterial.create(title: "Crypto for dummies",
+  description: "A short introduction to cryptocurrencies. Fully Idiot Proof.",
+  topic_material_id: crypto_guide.id
 )
+
+long_term_investing = TopicMaterial.create(topic_name: "investments")
+
+LearningMaterial.create(title: "In it For the Long Run",
+  description: " A detailed look into the intricacies of the stock market and how you can win over 5-10 years.",
+  topic_material_id: long_term_investing.id
+)
+
+debt_management_plans = TopicMaterial.create(topic_name: "debt")
+
+
+LearningMaterial.create(title: "Becoming Debt Free!",
+  description: "An article about debt management plans and how you can use them to manage your debt...",
+  topic_material_id: debt_management_plans.id
+)
+
+
+saving_principles = TopicMaterial.create(topic_name: "saving")
+
+LearningMaterial.create(title: "12 principles
+  for saving and investing",
+  description: "an article about 12 principles for saving. a short guide.",
+  topic_material_id: saving_principles.id
+)
+
+penny_stocks = TopicMaterial.create(topic_name: "investments")
+
+LearningMaterial.create(title: "What can I do With £50?",
+  description: "An insight into the volatile world of penny stocks and the money you can make with just £50",
+  topic_material_id: penny_stocks.id
+)
+
+banking = TopicMaterial.create(topic_name: "banking")
+
+LearningMaterial.create(title: "Banking overview",
+  description: "An overview of the Financial Services",
+  topic_material_id: banking.id
+)
+
+indexes = TopicMaterial.create(topic_name: "investments")
+
+LearningMaterial.create(title: "How to retire at 40",
+  description: "A quick look at how to compound your assets using indexes",
+  topic_material_id: indexes.id
+)
+
+spending = TopicMaterial.create(topic_name: "spending")
+
+LearningMaterial.create(title: "Best Ways to Spend Your Money in 2023",
+  description: "No More Gadgets!!! This article gives you a look into the best things to spend money on in 2023",
+  topic_material_id: spending.id
+)
+
+finlit = TopicMaterial.create(topic_name: "Tech")
+
+LearningMaterial.create(title: "Investment apps you need in 2023",
+  description: "A deep dive into the must-have finance apps in 2023 including a new high-flyer called FinLit",
+  topic_material_id: finlit.id
+)
+
+stonks = TopicMaterial.create(topic_name: "investments")
+
+LearningMaterial.create(title: "Top 5 stocks to buy now",
+  description: "A look at all the companies on the market with great fundamentals that will double your money in 2 years",
+  topic_material_id: stonks.id
+)
+
+
 
 puts "finalize seeds"
