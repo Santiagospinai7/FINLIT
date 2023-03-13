b65ac6b11c9fd3b6369924606cc29b1e56bc21ec
max whitehead <fifes-catkin.0r@icloud.com>
2023-03-13 16:48:55 +0000

    WORKING APIs AND ENVs

--- app/javascript/controllers/unsplash_controller.js
+++ app/javascript/controllers/unsplash_controller.js
@@ -2,30 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "image" ]
+  static values = {
+    key: String
+  }
 
   connect() {
-      console.log("REEEEEEE!");
-      this.imageTarget = this.imageTarget || this.targets.find("image");
-      const accessKey = "FboSVqbKlq0wIqv_ySxZklvUI6C6yqj0ooTP4gh9Ugg";
-      const apiUrl = '';
-      fetch(apiUrl, {
-        headers: {
-          'Accept-Version': 'v1',
-          Authorization: `Client-ID ${accessKey}`,
-        },
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.urls.regular);
-        this.imageTarget.src = data.urls.regular;
-      })
-        .catch(error => console.error(error));
-    }
+    console.log("UNSPLASH - KEY", this.keyValue);
+    const imageTarget = this.imageTarget || this.targets.find("image");
+    const accessKey = this.keyValue;
+    const apiUrl = 'https://api.unsplash.com/photos/random?query=finance';
+    fetch(apiUrl, {
+      headers: {
+        'Accept-Version': 'v1',
+        Authorization: `Client-ID ${accessKey}`,
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.urls.regular);
+      imageTarget.src = data.urls.regular;
+    })
+    .catch(error => console.error(error));
+  }
 
   refresh() {
     console.log("click");
-    const accessKey = "FboSVqbKlq0wIqv_ySxZklvUI6C6yqj0ooTP4gh9Ugg";
-    const apiUrl = '';
+    const accessKey = this.keyValue;
+    const apiUrl = 'https://api.unsplash.com/photos/random?query=finance';
     fetch(apiUrl, {
       headers: {
         'Accept-Version': 'v1',
